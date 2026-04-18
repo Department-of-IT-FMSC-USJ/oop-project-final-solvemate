3ef4576457424940cc22ccbba0d08da2b8c5e020
Avindi-Getawakanda <avindigetawakanda@gmail.com>
2026-04-18 11:14:12 +0530

    added a landing page.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AdminDashboard from "./pages/AdminDashboard";
@@ -20,22 +21,15 @@ function ProtectedUser({ children }: { children: React.ReactNode }) {
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/"          element={<Login />} />
-                <Route path="/register"  element={<Register />} />
-                <Route path="/admin"     element={
-                    <ProtectedAdmin>
-                        <AdminDashboard />
-                    </ProtectedAdmin>
-                } />
-                <Route path="/user"      element={
-                    <ProtectedUser>
-                        <UserDashboard />
-                    </ProtectedUser>
-                } />
-                <Route path="*"          element={<Navigate to="/" replace />} />
-            </Routes>
-        </BrowserRouter>
+         <BrowserRouter>
+                    <Routes>
+                        <Route path="/"         element={<LandingPage />} />
+                        <Route path="/login"    element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/admin"    element={<AdminDashboard />} />
+                        <Route path="/user"     element={<UserDashboard />} />
+                        <Route path="*"         element={<Navigate to="/" replace />} />
+                    </Routes>
+                </BrowserRouter>
     );
 }
